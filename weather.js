fb83e2228d39857ecf8a3bb9cf046312a36cf35c
G284222022 <228422@st.takushoku-u.ac.jp>
2023-06-01 15:22:01 +0900

    ss

--- weather.js
+++ weather.js
@@ -1,63 +1,81 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+let id_list = [
+  {city:"カイロ",        id:360630},
+  {city:"モスクワ",       id:524901},
+  {city:"ヨハネスブルク",  id:993800},
+  {city:"北京",  id:1816670},
+  {city:"東京",  id:1850147},
+  {city:"シンガポール",  id:1880252},
+  {city:"シドニー",  id:2147714},
+  {city:"ロンドン",  id:2643743},
+  {city:"パリ",  id:2968815},
+  {city:"リオデジャネイロ",  id:3451189},
+  {city:"ニューヨーク",  id:5128581},
+  {city:"ロサンゼルス",  id:5368361}
+]
 
 ////////// 課題3-2 ここからプログラムを書こう
-let we = document.querySelector('h3#weather'); 
-let p = document.createElement("h3");   
-text = "都市名:" + data.name + "   最高気温:" + data.main.temp_max + "   最低気温:" + data.main.temp_min; 
-p.textContent = text;
-we.insertAdjacentElement("afterend", p);
+let button1 = document.querySelector('#search');
+button1.addEventListener('click', searchs);
 
-let button = document.querySelector('#print');
-button.addEventListener('click', kensaku);
+let button2 = document.querySelector('#print');
+button2.addEventListener('click', kensaku);
+
 
 
 function kensaku(){
   let i = document.querySelector('input[name="target"]');
   let target = i.value;       // ユーザが記入した文字列
+}
+
+function searchs(){
+  for (let n of id_list){
+    sendRequest(n.id)
+  }
+}
+
+
+ 
+function sendRequest(id) {
+  // URL を設定
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
   
+  // 通信開始
+  axios.get(url)
+    .then(showResult)
+    .catch(showError)
+    .then(finish);
+  }
+
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+  
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+    data = JSON.parse(data);
+  }
+  
+  // data をコンソールに出力
+  console.log(data);
+  
+  // data.x を出力
+  console.log(data.x);
+}
+  
+// 通信エラーが発生した時の処理
+function showError(err) {
+  console.log(err);
+}	
+  
+// 通信の最後にいつも実行する処理
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
+
+function result(data){
+  let we = document.querySelector('h3#weather'); 
+  let p = document.createElement("h3");   
+  text = "都市名:" + data.name + "   最高気温:" + data.main.temp_max + "   最低気温:" + data.main.temp_min; 
+  p.textContent = text;
+  we.insertAdjacentElement("afterend", p);
 }
