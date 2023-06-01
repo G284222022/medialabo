da01a0501c97758b813cde7cfddb5d384c852d79
G284222022 <228422@st.takushoku-u.ac.jp>
2023-06-01 23:20:31 +0900

    kadai

--- weather.js
+++ weather.js
@@ -17,24 +17,52 @@ let id_list = [
 let button1 = document.querySelector('#search');
 button1.addEventListener('click', searchs);
 
-let button2 = document.querySelector('#print');
-button2.addEventListener('click', kensaku);
+let button2 = document.querySelector('#filter');
+button2.addEventListener('click', checkers)
 
+let checkbox0 = document.getElementById('kairo');
+let checkbox1 = document.getElementById('mosukuwa');
+let checkbox2 = document.getElementById('yohane');
+let checkbox3 = document.getElementById('pekin');
+let checkbox4 = document.getElementById('Tokyo');
+let checkbox5 = document.getElementById('Singa');
+let checkbox6 = document.getElementById('Sydney');
+let checkbox7 = document.getElementById('ron');
+let checkbox8 = document.getElementById('pari');
+let checkbox9 = document.getElementById('rio');
+let checkbox10 = document.getElementById('new');
+let checkbox11 = document.getElementById('ros');
 
-
-function kensaku(){
-  let i = document.querySelector('input[name="target"]');
-  let target = i.value;       // ユーザが記入した文字列
-}
 
 function searchs(){
+  h3reset();
   for (let n of id_list){
     sendRequest(n.id)
   }
 }
 
+function checkers(){
+  let checkboxlist = [
+    checkbox0, checkbox1, checkbox2, checkbox3,checkbox4,  checkbox5,
+    checkbox6, checkbox7, checkbox8, checkbox9, checkbox10, checkbox11
+  ];
+  h3reset();
+  let n  = 0;
+  for (i of checkboxlist){
+    if (i.checked){
+      sendRequest(id_list[n].id)
+    }
+    n += 1
+  }
+}
 
- 
+function h3reset(){
+  let div = document.querySelectorAll('div#weather > h3');
+  for (n of div){
+    n.remove()
+  }
+}
+
 function sendRequest(id) {
   // URL を設定
   let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
@@ -47,19 +75,11 @@ function sendRequest(id) {
   }
 
 function showResult(resp) {
-  // サーバから送られてきたデータを出力
   let data = resp.data;
-  
-  // data が文字列型なら，オブジェクトに変換する
   if (typeof data === 'string') {
     data = JSON.parse(data);
   }
-  
-  // data をコンソールに出力
-  console.log(data);
-  
-  // data.x を出力
-  console.log(data.x);
+  result(data);
 }
   
 // 通信エラーが発生した時の処理
@@ -72,10 +92,12 @@ function finish() {
   console.log('Ajax 通信が終わりました');
 }
 
-function result(data){
-  let we = document.querySelector('h3#weather'); 
+function result(datas){
+  let div = document.querySelector('div#weather');
   let p = document.createElement("h3");   
-  text = "都市名:" + data.name + "   最高気温:" + data.main.temp_max + "   最低気温:" + data.main.temp_min; 
+  text = "都市名:" + datas.name + "   最高気温:" + datas.main.temp_max + "   最低気温:" + datas.main.temp_min; 
   p.textContent = text;
-  we.insertAdjacentElement("afterend", p);
+  div.insertAdjacentElement("beforeend", p);
 }
+
+
